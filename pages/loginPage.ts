d9c159d71c02f6a1ae449aac7dc9afd2e9647566
MAvishkar12 <moreavishkar12@gmail.com>
2026-06-09 13:28:27 +0530

    POM created for Login Page

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -1,27 +1,32 @@
 import { Page, Locator } from "@playwright/test";
-
-import { UserData } from "../utils";
+//https://reqres.in/
+//Admin123
+//Admin123456@gmail.com
+// pass : Admin@123
 export class LoginPage {
-    page: Page;
-    userName: Locator;
-    passWord: Locator;
-    loginBtn: Locator;
+    readonly page: Page;
+    readonly loginText: Locator;
+    readonly emailId: Locator;
+    readonly password: Locator;
+    readonly loginBtn: Locator;
+
     constructor(page: Page) {
-        this.page = page
-        this.userName = page.getByPlaceholder('Username')
-        this.passWord = page.getByPlaceholder('Password')
+        this.page = page;
+
+        this.loginText = page.getByRole('link', { name: 'Signup / Login' });
+        this.emailId = page.getByPlaceholder('Email Address').first();
+        this.password = page.getByPlaceholder('Password');
         this.loginBtn = page.getByRole('button', { name: 'Login' });
-
     }
 
-    async gotoWebsite() {
-        await this.page.goto("https://opensource-demo.orangehrmlive.com/");
+    async pageGoTo() {
+        await this.page.goto('https://automationexercise.com/');
     }
 
-    async addUserCredentials() {
-        await this.userName.fill(UserData.Username)
-        await this.passWord.fill(UserData.Password)
-        await this.loginBtn.click()
+    async login(email: string, password: string) {
+        await this.loginText.click();
+        await this.emailId.fill(email);
+        await this.password.fill(password);
+        await this.loginBtn.click();
     }
 }
-
---
+++ tests/auth.spec.ts
@@ -0,0 +1,80 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from '../pages/LoginPage';
+
+test("Login User with correct email and password", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.pageGoTo();
+
+    await loginPage.login(
+        'Admin123456@gmail.com',
+        'Admin@123'
+    );
+    await expect(page.getByRole('link', { name: 'Logout' }))
+        .toBeVisible()
+})
+
+
+test('Login User with incorrect email and password', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.pageGoTo();
+
+    await loginPage.login(
+        'Admin123@gmail.com',
+        'Admin@123'
+    );
+    await expect(page.getByText('Your email or password is incorrect!')).toBeVisible()
+    await page.waitForTimeout(3000)
+
+})
+
+
+
+test('Register User with existing email', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.pageGoTo();
+    await page.getByRole('link', { name: 'Signup / Login' }).click()
+    await expect(page).toHaveURL(/login/)
+    await page.getByPlaceholder('Name').fill('Admin@123')
+    await page.getByPlaceholder('Email Address').nth(1).fill('Admin123456@gmail.com')
+    await page.getByRole('button', { name: 'Signup' }).click()
+    await expect(page.getByText('Email Address already exist!')).toBeVisible()
+
+})
+
+test("Contact Us Form", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.pageGoTo();
+    await page.getByText('Contact us').click()
+    await expect(page).toHaveURL(/contact_us/)
+    await page.getByRole('textbox', { name: 'Name' }).fill('abc')
+    await page.getByPlaceholder('Email').first().fill('Admin123456@gmail.com')
+    await page.getByPlaceholder('Subject').fill("Replace product and refund amount")
+    await page.getByPlaceholder('Your Message Here').fill('replace product and get refund money on give account details')
+    const fileInput = page.locator('input[type="file"]');
+    await fileInput.setInputFiles('./files/dumy.png');
+
+    page.on('dialog', async dialog => {
+        console.log(dialog.message());
+        await dialog.accept();
+    });
+
+    await page.locator('[name="submit"]').click();
+})
+
+test(' Logout User', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.pageGoTo();
+
+    await loginPage.login(
+        'Admin123456@gmail.com',
+        'Admin@123'
+    );
+    await expect(page.getByRole('link', { name: 'Logout' }))
+        .toBeVisible()
+    await page.locator('li:has-text("Logout")').click()
+    await expect(page).toHaveURL(/login/)
+})
+
