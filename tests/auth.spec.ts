c1920c42d1ca446bf3037665480a16051d4473b2
MAvishkar12 <moreavishkar12@gmail.com>
2026-06-12 20:51:21 +0530

    LoginPage import issue resolve

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
-import { LoginPage } from '../pages/LoginPage';
-import { UserData ,IncorrectNewUser } from "../utils";
 
+import { UserData ,IncorrectNewUser } from "../utils";
+import { LoginPage } from "../pages/LoginPage";
 test("Login User with correct email and password", async ({ page }) => {
     const loginPage = new LoginPage(page);
 
