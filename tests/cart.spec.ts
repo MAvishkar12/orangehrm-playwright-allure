ac8ec70c6a66d56dde390cec1314b43acc3810b0
MAvishkar12 <moreavishkar12@gmail.com>
2026-06-24 20:21:48 +0530

    cart and product review test done

---
+++ tests/cart.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/LoginPage";
+import { UserData } from "../utils";
+test(' Search Products and Verify Cart After Login', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.pageGoTo();
+    await page.getByRole('link', { name: ' Products' }).click()
+    await page.getByPlaceholder('Search Product', { exact: true }).fill('Blue')
+    await page.locator('button.btn.btn-default.btn-lg').click()
+    await expect(page.locator(".title.text-center")).toBeVisible()
+    const products = page.locator('.single-products');
+    const count = await products.count();
+    for (let i = 0; i < count; i++) {
+        const productName = await page.locator('.productinfo p').nth(i).innerText();
+        expect(productName.toLowerCase()).toContain('blue');
+    }
+    for (let i = 0; i < count; i++) {
+        const product = products.nth(i);
+        await product.hover();
+        await product
+            .locator('a.add-to-cart')
+            .first()
+            .click();
+        await page.getByText('Continue Shopping').click();
+    }
+    await page.getByText('Cart', { exact: true }).click()
+    const table = await page.locator('#cart_info_table')
+    const tableRowCount = await table.locator('tbody tr').count()
+    await expect(tableRowCount).toEqual(count)
+    await page.getByRole('link', { name: 'Signup / Login' }).click()
+    await loginPage.login(UserData.email, UserData.password)
+    await page.getByText('Cart', { exact: true }).click()
+    await expect(tableRowCount).toEqual(count)
+}) 
---
+++ tests/productReview.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../pages/LoginPage";
+test(' Add review on product', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.pageGoTo();
+    await page.getByRole('link', { name: ' Products' }).click();
+    await page.getByRole('link', { name: ' View Product' }).first().click();
+    await page.getByRole('textbox', { name: 'Your Name' }).fill('Admin');
+    await page.getByRole('textbox', { name: 'Email Address', exact: true }).fill('admin12@gmail.com');
+    await page.getByRole('textbox', { name: 'Add Review Here!' }).fill('Prodcut is good add some colors');
+    await page.getByRole('button', { name: 'Submit' }).click();
+    await expect(page.getByText('Thank you for your review.')).toBeVisible()
+})
